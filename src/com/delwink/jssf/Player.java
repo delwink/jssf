38c19fa21ec584f32a46c86b54ed9a3f4d251c15
David McMackins II <contact@mcmackins.org>
2015-12-25 20:54:15 -0600

    Implement basic player

--- src/com/delwink/jssf/Player.java
+++ src/com/delwink/jssf/Player.java
@@ -14,12 +14,58 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.delwink.jssf;
 
 /**
- *
- * @author the pc
+ * The player character.
+ * @author David McMackins II
  */
 public class Player {
+    private final String TEXTURE_NAME;
+    private float x, y, w, h;
     
+    public Player(String textureName, float x, float y, float w, float h) {
+        this.TEXTURE_NAME = textureName;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    
+    public String getTextureName() {
+        return this.TEXTURE_NAME;
+    }
+
+    public float getX() {
+        return this.x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return this.y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getW() {
+        return this.w;
+    }
+
+    public void setW(float w) {
+        this.w = w;
+    }
+
+    public float getH() {
+        return this.h;
+    }
+
+    public void setH(float h) {
+        this.h = h;
+    }
 }
