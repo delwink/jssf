3306b9e1e7582320b9cb9aef80b3ef099a3c82ad
David McMackins II <contact@mcmackins.org>
2016-01-02 11:19:38 -0600

    Bump copyright date to reflect recent changes

--- src/com/delwink/jssf/geom/Rectangle.java
+++ src/com/delwink/jssf/geom/Rectangle.java
@@ -1,6 +1,6 @@
 /*
  * jssf - Java Side Scroller Format library
- * Copyright (C) 2015 Delwink, LLC
+ * Copyright (C) 2015-2016 Delwink, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
